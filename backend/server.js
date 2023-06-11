d02d71c3cd350c65837d0d4bb81c8f58cd469cda
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-06-11 17:06:29 +0530

    PAYPAL integration

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 connectDB() //Connect to MongoDB
 
@@ -29,6 +29,8 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+
+app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }))
 
 app.use(notFound)
 app.use(errorHandler)
