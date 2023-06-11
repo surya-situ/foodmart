5e2fdf44b833ce8a9541f0f957502e327c3f4853
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-06-11 17:04:17 +0530

    product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -19,5 +19,50 @@ const getProductById = asyncHandler( async (req, res) => {
     }
 });
 
+//?CREATE A PRODUCT
+//!ADMIN
+const createProduct = asyncHandler( async (req, res) => {
+    const product = new Product({
+        name: 'sample name',
+        price: 0,
+        user: req.user._id,
+        image: '/images/sample.jpg',
+        brand: 'sample brand',
+        category: 'sample category',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'sample desc'
+    })
 
-export {getProducts, getProductById};
+    const createProduct = await product.save();
+    res.status(201).json(createProduct)
+});
+
+
+//?UPDATE A PRODUCT
+//!ADMIN   
+const updateProduct = asyncHandler( async (req, res) => {
+    const { name, price, description, image, brand, category, countInStock } = req.body
+
+    const product = await Product.findById(req.params.id)
+
+    if(product) {
+        product.name = name,
+        product.price = price,
+        product.description = description,
+        product.image = image,
+        product.brand = brand,
+        product.category = category,
+        product.countInStock = countInStock
+
+        const updateProduct = await product.save();
+
+        res.json(updateProduct)
+    } else {
+        res.status(404);
+        throw new Error('Resource not found')
+    }
+});
+
+
+export {getProducts, getProductById, createProduct, updateProduct};
