9a3df08521bd349979a4acfc3ef9cb95d6e18767
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-06-11 17:13:32 +0530

    Order api slice

--- frontend/src/slices/orderApiSlice.js
+++ frontend/src/slices/orderApiSlice.js
@@ -11,14 +11,58 @@ export const orderApiSlice = apiSlice.injectEndpoints({
             })
         }),
 
+        // getOrderDetails: builder.query({
+        //     query: (orderId) => ({
+        //         url: `${ORDERS_URL}/${orderId}`,
+        //     }),
+        //     keepUnusedDataFor: 5
+        // }),
+
         getOrderDetails: builder.query({
-            query: (orderId) => ({
-                url: `${ORDERS_URL}/${orderId}`,
+            query: (id) => ({
+              url: `${ORDERS_URL}/${id}`,
+            }),
+            keepUnusedDataFor: 5,
+        }),
+
+        payOrder: builder.mutation({
+            query: ({orderId, details}) => ({
+                url: `${ORDERS_URL}/${orderId}/pay`,
+                method: 'PUT',
+                body: {...details}
+            })
+        }),
+
+        getPayPalClientId: builder.query({
+            query: () => ({
+                url: PAYPAL_URL
+            }),
+            keepUnusedDataFor: 5,
+        }),
+
+        getMyOrders: builder.query({
+            query: () => ({
+                url: `${ORDERS_URL}/mine`
             }),
             keepUnusedDataFor: 5
         }),
+
+        getOrders: builder.query({
+            query: () => ({
+                url: ORDERS_URL
+            }),
+            keepUnusedDataFor: 5
+        }),
+
+        deliverOrder: builder.mutation({
+            query: (orderId) => ({
+                url: `${ORDERS_URL}/${orderId}/deliver`,
+                method: 'PUT'
+            }),
+
+        })
     })
 })
 
 
-export const { useCreateOrderMutation, useGetOrderDetailsQuery } = orderApiSlice;
+export const { useCreateOrderMutation, useGetOrderDetailsQuery, usePayOrderMutation, useGetPayPalClientIdQuery, useGetMyOrdersQuery, useGetOrdersQuery, useDeliverOrderMutation } = orderApiSlice;
