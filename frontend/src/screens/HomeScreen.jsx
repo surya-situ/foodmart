57cc96434572b259375ff3b24ed14d9609c187d5
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-06-13 09:06:31 +0530

    carousel component

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 // import React,{useEffect, useState} from 'react'
 // import axios from 'axios'
 import {Row, Col} from 'react-bootstrap'
+import { FaHamburger } from 'react-icons/fa';
+import {Button} from 'react-bootstrap'
 import Product from '../components/Product'
 import { useGetProductsQuery } from '../slices/productApiSlice';
-import Hero from '../components/Hero';
+import { Link } from 'react-router-dom'
+import ProductCarousel from '../components/ProductCarousel';
+
 
 const HomeScreen = () => {
 
@@ -23,14 +27,19 @@ const HomeScreen = () => {
 
     return (
         <>
-
+            <ProductCarousel />
             { isLoading ? ( <h2>Loading</h2> ) : error ? ( <div>{ error?.data?.message || error.error }</div> ) : (
                 <>
-                    <Hero />
-                    <h1>Popular Products</h1>
+                    {/* <Hero /> */}
+                    <Col>
+                        <h1>Popular Products</h1>
+                        <Link to='/menu' style={{ textDecoration: 'none' }}>
+                            <Button bg='dark' rounded='true'><FaHamburger /> See all items</Button>
+                        </Link>
+                    </Col>
                     <Row>
                         {
-                            products.map( (product) => (
+                            products.slice(0, 4).map( (product) => (
                                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                                     <Product product={product} />
                                 </Col>
