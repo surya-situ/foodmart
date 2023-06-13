28cdd3a3def08b5954585e71791d92a3f2641f3d
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-06-13 09:03:57 +0530

    new features in menu screen

---
+++ frontend/src/screens/MenuScreen.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Row, Col} from 'react-bootstrap'
+import Product from '../components/Product'
+import { useGetProductsQuery } from '../slices/productApiSlice';
+
+const HomeScreen = () => {
+
+
+    const {data: products, isLoading, error} = useGetProductsQuery();
+
+
+    return (
+        <>
+
+            { isLoading ? ( <h2>Loading</h2> ) : error ? ( <div>{ error?.data?.message || error.error }</div> ) : (
+                <>
+                    
+                    <h3>All Items</h3>
+                    <Row>
+                        {
+                            products.map( (product) => (
+                                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                    <Product product={product} />
+                                </Col>
+                            ))
+                        }
+                    </Row>
+                </>
+            )}
+            
+        </>
+    )
+}
+
+export default HomeScreen
