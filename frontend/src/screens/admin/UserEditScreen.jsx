9d7ff8f0ed40514682696212b1f19ea2a8d53eeb
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-06-13 08:53:47 +0530

    user details

---
+++ frontend/src/screens/admin/UserEditScreen.jsx
@@ -0,0 +1,110 @@
+import { useState, useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+import Message from '../../components/Message';
+import Loader from '../../components/Loader';
+import FormContainer from '../../components/FormContainer';
+import { toast } from 'react-toastify';
+import {
+  useGetUserDetailsQuery,
+  useUpdateUserMutation,
+} from '../../slices/usersApiSlice';
+
+const UserEditScreen = () => {
+  const { id: userId } = useParams();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const {
+    data: user,
+    isLoading,
+    refetch,
+    error,
+  } = useGetUserDetailsQuery(userId);
+
+  const [updateUser, { isLoading: loadingUpdate }] =
+    useUpdateUserMutation();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+      setEmail(user.email);
+      setIsAdmin(user.isAdmin);
+    }
+  }, [user]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      await updateUser({ userId, name, email, isAdmin });
+      toast.success('user updated successfully');
+      refetch();
+      navigate('/admin/userlist');
+    } catch (error) {
+      toast.error(error?.data?.message || error.error);
+    }
+  };
+
+
+  return (
+    <>
+      <Link to='/admin/userlist' className='btn btn-light my-3'>
+        Go Back
+      </Link>
+      <FormContainer>
+        <h1>Edit user</h1>
+        {loadingUpdate && <Loader />}
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant='danger'>{error.message}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId='name'>
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type='name'
+                placeholder='Enter name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='email'>
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type='email'
+                placeholder='Enter email address'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group className='my-2' controlId='isadmin'>
+              <Form.Check
+                type='checkbox'
+                label='Is Admin'
+                checked={isAdmin}
+                onChange={(e) => setIsAdmin(e.target.checked)}
+              ></Form.Check>
+            </Form.Group>
+
+            <Button
+              type='submit'
+              variant='primary'
+              style={{ marginTop: '1rem' }}
+            >
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
+  );
+};
+
+export default UserEditScreen;
