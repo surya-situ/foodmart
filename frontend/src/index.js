015a083282a62956d4bad5dec80c02473840cf8e
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-06-13 09:03:16 +0530

    Private admin routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,7 @@ import reportWebVitals from './reportWebVitals';
 import PrivateRoute from './components/PrivateRoute';
 import AdminRoute from './components/AdminRoute';
 import HomeScreen from './screens/HomeScreen';
+import MenuScreen from './screens/MenuScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
@@ -24,11 +25,15 @@ import ProfileScreen from './screens/ProfileScreen';
 import OrderListScreen from './screens/admin/OrderListScreen';
 import ProductListScreen from './screens/admin/ProductListScreen';
 import ProductEditScreen from './screens/admin/ProductEditScreen';
+import UserListScreen from './screens/admin/UserListScreen';
+import UserEditScreen from './screens/admin/UserEditScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={ <App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/menu' element={<MenuScreen />} />
+      <Route path='/menu/page/:pageNumber' element={<MenuScreen />} />
       <Route path='/product/:id' element={<ProductScreen />} />
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />} />
@@ -48,6 +53,8 @@ const router = createBrowserRouter(
         <Route path='/admin/orderlist' element={<OrderListScreen />} />
         <Route path='/admin/productlist' element={<ProductListScreen />} />
         <Route path='/admin/product/:id/edit' element={<ProductEditScreen />} />
+        <Route path='/admin/userlist' element={<UserListScreen />} />
+        <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
       </Route>
 
     </Route>
