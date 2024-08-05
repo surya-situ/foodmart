4beab61e9fa7c418e79f00166039b008c0645f0d
surya-situ <suryasocial55@gmail.com>
2024-08-05 11:54:56 +0530

    feat: Implimented logic to run it in producion mode

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,6 @@ app.use(express.urlencoded({extended: true}))
 //Cookie parser
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
-    res.send('api running')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -36,7 +32,19 @@ app.use('/api/upload', uploadRoutes)
 app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }))
 
 const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if ( process.env.NODE_ENV === 'production' ) {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send('api running')
+    });
+}
 
 app.use(notFound)
 app.use(errorHandler)
